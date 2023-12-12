13a0580cd3017d042075f10fa5ca36718a09c524
Masum Billah <95751615+masum200119@users.noreply.github.com>
2023-12-12 21:31:44 +0600

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php session_start();
+define('DBNAME','tute');
+define('DBUSER','root');
+define('DBPASS','');
+define('DBHOST','localhost');
+try {
+  $db = new PDO("mysql:host=".DBHOST.";dbname=".DBNAME, DBUSER, DBPASS);
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  //echo "Your page is connected with database successfully..";
+} catch(PDOException $e) {
+  echo "Issue -> Connection failed: " . $e->getMessage();
+}
+?>
